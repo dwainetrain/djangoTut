968403e78d043fc608238af2f1f50b8fa2455862
dwainetrain <dwaine.best@gmail.com>
2018-01-14 11:37:18 -0800

    A little more on testing. Admin considerations. And finishing out the Tut. Need to focus on commiting more often.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -16,11 +16,17 @@ class IndexView(generic.ListView):
         Return the last five published question
         (not including those set to be published in the future)
         """
-        return Question.objects.filter(pub_date_lte=timezone.now()).order_by('-pubdate')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Exclures any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
